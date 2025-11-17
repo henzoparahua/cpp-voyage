841e3fbba8cfe2895370504525863823ed935f59
henzoparahua <hnz.2894henzop19@gmail.com>
2025-11-17 11:57:24 -0300

    Nested functions are not allowed in C++.

--- Print-Something/main.cpp
+++ Print-Something/main.cpp
@@ -7,7 +7,18 @@ void printSomething() {
 
 int main()
 {
+	/* Nested functions are not allowed in C++.
+	   Functions must be defined at the global scope or within a class/struct.
+
+	   void nestedFunction() {
+		   std::cout << "This is a nested function." << std::endl;
+	   }
+	
+	*/
+
 	std::cout << "Start of the program" << std::endl;
+	printSomething();
+	printSomething();
 	printSomething();
 	std::cout << "End of the program" << std::endl;
 
